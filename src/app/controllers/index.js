7e14c10df5a2a09e811824b59fedaba4b9b506d3
Silvio Moreira Galvão Junior <silviojuniorempresa@gmail.com>
2023-01-03 00:08:30 -0300

    get customers from database

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -3,13 +3,17 @@ const port = 3333
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const Customer = require('../models/Customer.js')
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.get('/customers', (req, res, next) => {
-    res.send(`{"customer": {"nome": "Teste"}}`)
+app.get('/customers', async (req, res, next) => {
+    const customers = await Customer.findAll()
+    res.send(customers)
 })
 
 app.listen(port, () => {
     console.log(`Server running at port: ${port}.`)
 })
+
+
