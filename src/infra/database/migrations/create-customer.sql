7f6437f180ed65e53d3dec1b69be0cde00bb8f3a
Silvio Moreira Galvão Junior <silviojuniorempresa@gmail.com>
2023-01-22 11:05:48 -0300

    refactoring tables

--- src/infra/database/migrations/create-customer.sql
+++ src/infra/database/migrations/create-customer.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS clientes (
-    id_clientes INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     nome VARCHAR (100) NOT NULL,
     sobrenome VARCHAR (100) NOT NULL,
     nascimento DATE NOT NULL,
-    id_enderecos INT UNSIGNED NOT NULL,
+    id INT UNSIGNED NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (enderecos) REFERENCES  enderecos(id_enderecos)
+    
 );
--- src/infra/database/migrations/create-street.sql
+++ src/infra/database/migrations/create-street.sql
@@ -1,11 +1,9 @@
-CREATE TABLE IF NOT EXISTS  enderecos ( 
-    id_enderecos INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS  endereco ( 
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     logradouro VARCHAR (100) NOT NULL,
     bairro VARCHAR (100) NOT NULL,
     cidade VARCHAR (100) NOT NULL,
     estado VARCHAR (2) NOT NULL,
 	cep VARCHAR (9) NOT NULL,
-    id_cliente INT UNSIGNED NOT NULL,
 	PRIMARY KEY (id),
-    FOREIGN KEY (cliente) REFERENCES cliente (id_clientes)
 );
