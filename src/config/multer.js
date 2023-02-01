1e29db1c6b2b8395539e98c035af437d81a5544c
Silvio Moreira Galvão Junior <silviojuniorempresa@gmail.com>
2023-02-01 20:45:07 -0300

    feat: add multer image file filter

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,5 +1,6 @@
 const multer = require('multer');
 const { v4: uuidv4} = require('uuid');
+const path = require('path');
 
 module.exports = {
     dest: 'temp/uploads/',
@@ -11,5 +12,12 @@ module.exports = {
             const prefix = uuidv4()
             cb(null, `${prefix}_${file.originalname}`)
         }
-    })
+    }),
+    fileFilter: function (req, file, callback) {
+        const ext = path.extname(file.originalname);
+        if(ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
+            return callback(new Error('Only images are allowed'))
+        }
+        callback(null, true)
+    }
 }
